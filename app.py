5c160f72d70783a9427680e657317664a90c5b2e
Muhanmmad Saad <saad992.sm@gmail.com>
2024-07-08 22:17:48 +0500

    update path

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
+import uuid
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
-import os
-import uuid
+from moviepy.editor import VideoFileClip, AudioFileClip
+
 from api import infer
 
 app = FastAPI()
 
 @app.post("/generate_video/")
 async def generate_video(image: UploadFile = File(...), audio: UploadFile = File(...)):
-    image_path = f"/home/saad/Projects/SadTalker/sadtalker/tmp/{uuid.uuid4()}_{image.filename}"
+    image_path = f"tmp/{uuid.uuid4()}_{image.filename}"
     with open(image_path, "wb") as f:
         f.write(image.file.read())
 
-    audio_path = f"/home/saad/Projects/SadTalker/sadtalker/tmp/{uuid.uuid4()}_{audio.filename}"
+    audio_path = f"tmp/{uuid.uuid4()}_{audio.filename}"
     with open(audio_path, "wb") as f:
         f.write(audio.file.read())
 
     # Assuming `sadtalker.process` is the function to generate video
-    output_video_path = f"/home/saad/Projects/SadTalker/sadtalker/tmp/{uuid.uuid4()}_output.mp4"
+    output_video_path = f"tmp/{uuid.uuid4()}_output.mp4"
     
     output_path = infer(audio_path, image_path)
 
