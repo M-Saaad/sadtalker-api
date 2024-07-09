5096e57c2827aacc59e43f736e778a5d5a00f621
Muhanmmad Saad <saad992.sm@gmail.com>
2024-07-10 00:42:29 +0500

    add args

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
+import os
 import uuid
-from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, UploadFile, Form
+from starlette.background import BackgroundTask
 from moviepy.editor import VideoFileClip, AudioFileClip
 
 from api import infer
 
 app = FastAPI()
 
+def cleanup_file(file_path: str):
+    os.remove(file_path)
+    print(f"File {file_path} has been deleted.")
+
 @app.post("/generate_video/")
-async def generate_video(image: UploadFile = File(...), audio: UploadFile = File(...)):
+async def generate_video(
+    image: UploadFile,
+    audio: UploadFile,
+    pose_style: int = File(0),
+    batch_size: int = File(2),
+    expression_scale: float = File(1.0),
+    preprocess: str = File('crop'),
+    still: bool = File(False),
+    enhancer: str = File(None)
+):
+
     image_path = f"tmp/{uuid.uuid4()}_{image.filename}"
     with open(image_path, "wb") as f:
         f.write(image.file.read())
@@ -17,12 +33,22 @@ async def generate_video(image: UploadFile = File(...), audio: UploadFile = File
     with open(audio_path, "wb") as f:
         f.write(audio.file.read())
 
-    # Assuming `sadtalker.process` is the function to generate video
-    output_video_path = f"tmp/{uuid.uuid4()}_output.mp4"
+    print(image,audio,pose_style,batch_size,expression_scale,preprocess,still,enhancer)
     
-    output_path = infer(audio_path, image_path)
+    output_path = infer(
+        driven_audio=audio_path,
+        source_image=image_path,
+        pose_style=pose_style,
+        batch_size=batch_size,
+        expression_scale=expression_scale,
+        preprocess=preprocess,
+        still=still,
+        enhancer=enhancer
+    )
 
-    return FileResponse(output_path+'.mp4')
+    background_task = BackgroundTask(cleanup_file, output_path+".mp4")
+
+    return FileResponse(output_path+".mp4", background=background_task)
 
 if __name__ == "__main__":
     import uvicorn
